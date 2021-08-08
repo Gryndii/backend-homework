ec2e247c9ad27352dc870d9e5bd713b22a9a549a
Roman Gryndii <r.gryndii@gmail.com>
2021-08-08 14:51:19 +0300

    task 1 started

---
+++ lesson-2/task-1.js
@@ -0,0 +1,35 @@
+class TimersManager {
+  constructor() {
+    this.timers = [];
+  }
+
+  add() {}
+  remove() {}
+  start() {}
+  stop() {}
+  pause() {}
+  resume() {}
+
+}
+
+const manager = new TimersManager();
+
+const t1 = {
+  name: 't1',
+  delay: 1000,
+  interval: false,
+  job: () => { console.log('t1') }
+};
+
+const t2 = {
+  name: 't2',
+  delay: 1000,
+  interval: false,
+  job: (a, b) => a + b
+};
+
+manager.add(t1);
+manager.add(t2, 1, 2);
+manager.start();
+console.log(1);
+manager.pause('t1');
